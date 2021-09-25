504be9462850b622ffb1f5b74203d6ad2345bf8a
ro <ro.michi1129@gmail.com>
2021-09-25 13:13:54 +0900

    66. ルーティング

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { ChakraProvider, Button } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./router/Router";
 import theme from "./theme/theme";
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Button colorScheme="teal">ボタン</Button>
-      <p>aaaaaa</p>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ChakraProvider>
   );
 }

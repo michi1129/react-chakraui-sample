547b40df7b9252d9f7e84f6beb78c1717e2bbf3e
ro <ro.michi1129@gmail.com>
2021-09-25 18:46:48 +0900

    75. ユーザー一覧取得の機能を追加する

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,17 +1,34 @@
+import { memo, useEffect, VFC } from "react";
 import { Wrap, WrapItem } from "@chakra-ui/react";
-import { memo, VFC } from "react";
+import { Spinner } from "@chakra-ui/spinner";
+import { Center } from "@chakra-ui/layout";
+import { useAllUsers } from "../../hooks/useAllUsers";
 import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, users, loading } = useAllUsers();
+
+  useEffect(() => getUsers(), []);
+
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl="https://source.unsplash.com/random"
-          userName="NAME"
-          fullName="FULLNAME"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center height="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });

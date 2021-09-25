c1ff5baee7c1549e6585524083857045058de6d3
ro <ro.michi1129@gmail.com>
2021-09-25 18:28:48 +0900

    74. ユーザー一覧のコンポーネントを追加する

---
+++ src/components/organisms/user/UserCard.tsx
@@ -0,0 +1,41 @@
+import { memo, VFC } from "react";
+import { Image } from "@chakra-ui/react";
+import { Box, Stack, Text } from "@chakra-ui/layout";
+
+type Props = {
+  imageUrl: string;
+  userName: string;
+  fullName: string;
+};
+
+export const UserCard: VFC<Props> = memo((props) => {
+  const { imageUrl, userName, fullName } = props;
+
+  return (
+    <Box
+      w={260}
+      h={260}
+      bg="white"
+      borderRadius="10px"
+      shadow="md"
+      p={4}
+      _hover={{ cursor: "pointer", opacity: 0.8 }}
+    >
+      <Stack textAlign="center">
+        <Image
+          boxSize="160px"
+          borderRadius="full"
+          src={imageUrl}
+          alt={userName}
+          m="auto"
+        />
+        <Text fontSize="lg" fontWeight="bold">
+          {userName}
+        </Text>
+        <Text fontSize="sm" color="gray">
+          {fullName}
+        </Text>
+      </Stack>
+    </Box>
+  );
+});
